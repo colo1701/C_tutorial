1b8c4ed9a66192f360e1f6a1541e72170c84beb9
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-09 20:39:17 +0100

    Update and rename 0003_data_types_2.c to 0003_data_type.c

---
+++ 0003_data_type.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  unsigned int franzjosef = -2000000000;
+  int hans;
+  
+  hans = 1337;
+  
+  int doerte = franzjosef + hans;
+  
+  printf("franzjosef + hans = %d\n", doerte);
+  
+  return 0;
+  
+}
