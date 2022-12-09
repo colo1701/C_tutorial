78ab877c9cdca6ce511640b3cb452142e7ebe2b5
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-09 21:43:06 +0100

    Update 0006_data_type.c

--- 0006_data_type.c
+++ 0006_data_type.c
@@ -8,8 +8,17 @@ int main() {
   
     /* 
     In this case the sum of an integer and a float is built and stored as a float again.
-    So one could expect to get a result of exactly 1339.800000. But actually the value one gets is 1339.00049
+    So one could expect to get a result of exactly 1339.800000. But actually the value one gets is 1339.00049.
+    This happens because a simple float only has a size of 4 Bytes. But there's a nother type of float: double
+    double decribes a float with a size of 8 Bytes which comes up with a higher accuracy.
     */
-    printf("%f\n", abc);
+    
+    double franzjosef8 = 2.8;
+    int hans8 = 1337;
+    
+    double abc8 = franzjosef8 + hans8;
+    
+    printf("The float value: %f\n", abc);
+    printf("The double value: %f\n", abc8);
     return 0; 
 }
