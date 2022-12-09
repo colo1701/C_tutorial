764191fbdb5bbbebee5cc056da49beb7c1821add
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-09 20:42:43 +0100

    Update 0002_formatted_print.c

--- 0002_formatted_print.c
+++ 0002_formatted_print.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  // Integer definieren (Größe 4 Byte)
+  // Define Integer variables (size 4 Bytes)
   int franzjosef = 42;
   int hans;
   hans = 1337;
   
   int doerte = franzjosef + hans;
   
+  // %d returns Integer value
   printf("%d", doerte);
   
   return 0;
