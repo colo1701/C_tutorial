73b8e7bcc49e6ad0dbd92ee8330959c390dd4cbb
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-09 21:56:53 +0100

    Create 0007_data_type.c

---
+++ 0007_data_type.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char abc = 'c';
+    
+    /*
+    %c returns a char output. But every char is also equal to a number, which can be seen
+    asking for the integer output of a char. These values can be seen in the ASCII table.
+    One can also do simple calculations using chars.
+    Here it can be seen again that exceeding the range of a calles data type doesn't cause an error 
+    but a senseless result so one should always be careful handling with those things.
+    */
+  
+    int hans = 1337;
+    int jupp = abc + hans;
+    
+    printf("I'm a char: %c\n", abc);
+    printf("I'm the respective integer: %d\n", abc);
+    printf("I'm the integer sum of 1337 and the letter c: %d\n", jupp);
+    printf("I'm the char sum of 1337 and the letter c: %c\n", jupp);
+    return 0; 
+}
