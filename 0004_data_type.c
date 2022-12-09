f35e531b08c0e34d3999ae1dce43e410d1f96698
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-09 20:59:49 +0100

    Update 0004_data_type.c

--- 0004_data_type.c
+++ 0004_data_type.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  unsigned int franzjosef = -2000000000;
-  int hans;
+  // Defining a float variable (size is 4 Bytes as well as the integer's size)
+  float franzjosef = 2.3;
+  float hans = 4.5;
   
-  hans = 1337;
+  float inge = franzjosef + hans;
   
-  int doerte = franzjosef + hans;
-  
-  /*
-  If %d is called in the print argument, it will handle franzjosef as an integer and return -1,999,998,663.
-  This case is trivial.
-  Changing the output variable type to %u (which stands for unsigned integer) will give a result of 2,294,968,633.
-  This happens because the value of a variable will loop through it's range if it exceeds it's data 
-  types value limit and restart from the opposite end of this range.
-  In this case the unsigned int value of franzjosef will restart counting down from 4,294,967,295 once it
-  falls below 0 which already happens when it's defined. So the initial value as an unsigned integer is
-  4,294,967,295 - 1,999,999,999 = 2,294,967,296
-  This comes to a total result of 
-  2,294,967,296 + 1,337 = 2,294,968,633
-  */
-  printf("franzjosef + hans = %d\n", doerte);
-  
+  // %f calls a float value as output
+  printf("%f\n", inge);
   return 0; 
 }
