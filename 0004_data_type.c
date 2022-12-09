0ead95b0cb6e05a8952bf3538132b158f9281785
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-09 21:09:57 +0100

    Update 0004_data_type.c

--- 0004_data_type.c
+++ 0004_data_type.c
@@ -7,7 +7,14 @@ int main() {
   
     float inge = franzjosef + hans;
   
-    // %f calls a float value as output
+    /* 
+    %f calls a float value as output.
+    If one changes the output back to %d (integer), it can happen, depending on the compiler used, 
+    that one either gets an error or a meaningless integer value is output (or even both).
+    So it is important to use data types consistently in any case, because unlike more modern 
+    programming languages, C often simply executes code instead of rejecting less meaningful statements 
+    with easy-to-understand error messages.
+    */
     printf("%f\n", inge);
     return 0; 
 }
