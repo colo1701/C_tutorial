0034f45303c4ffbb58c874e48373292823320939
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-09 21:03:02 +0100

    Update 0004_data_type.c

--- 0004_data_type.c
+++ 0004_data_type.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  // Defining a float variable (size is 4 Bytes as well as the integer's size)
-  float franzjosef = 2.3;
-  float hans = 4.5;
+    // Defining a float variable (size is 4 Bytes as well as the integer's size)
+    float franzjosef = 2.3;
+    float hans = 4.5;
   
-  float inge = franzjosef + hans;
+    float inge = franzjosef + hans;
   
-  // %f calls a float value as output
-  printf("%f\n", inge);
-  return 0; 
+    // %f calls a float value as output
+    printf("%f\n", inge);
+    return 0; 
 }
