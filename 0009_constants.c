71039f40d9d740fa5d0d579773ee7763c8ba9747
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-10 11:44:58 +0100

    Create 0009_constants.c

---
+++ 0009_constants.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+#define sets up a global constant variable or a so called pre-processor directive.
+This means they are set up before anything else happens. Variables set up like this can not be
+re-defined or changed within methods.
+*/
+
+#define PI 3.14159
+
+int main() {
+    float radius = 3.5;
+    
+    printf("This is Pi: %f\n", PI);
+    printf("This is the area of a circle with radius %f: %f\n", radius, radius*radius*PI);
+      
+    return 0; 
+}
