ebcb7e6e7381a934715df9a975fe6fc9b2c98646
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-11 01:05:36 +0100

    Update 0010_if_query.c

--- 0010_if_query.c
+++ 0010_if_query.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!");
-      
-    return 0;
+    int a = 0;
+    int b = 2;
+    
+    /*
+    The if query consists of 3 elements: the call, the condition and the command if the condition is true.
+    If the condition is the question whether 2 values are equal, it has to be asked using '==' because a single '='
+    is already used to define a value to a variable.
+    The simplest query asks for a single condition and will only return the command if the condition is true.
+    If the condition is false, the query will not cause any reply.
+    This query can be expanded by an 'else if' condition (if there's another condition to check) or an 'else'
+    argument (a reply that will show up, if none of the previous conditions are true).
+    */
+    
+    // This shows a simple if query:
+    if (a == 0) {
+        printf("a is 0.\n");    
     }
+    
+    // This shows a slightly more complex if query:
+    if (b == 0) {
+        printf("b is 0.\n");    
+    }
+    else if (b == 1) {
+        printf("b is 1.\n");
+    }
+    else {
+        printf("b is neither 0 nor 1.\n");
+    }
+    return 0;
+}
