68a99263dde5317692231616a7dbfeeb4e801d37
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-16 09:32:49 +0100

    Update 0005_data_type.c

--- 0005_data_type.c
+++ 0005_data_type.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    float franzjosef = 2.8;
-    float hans = 4.3;
+    float a = 2.8;
+    float b = 4.3;
   
-    int abc = franzjosef + hans;
+    int c = a + b;
   
     /* 
     Adding one (ore more) float values into an integer value will cause the integer variable to 
@@ -12,6 +12,6 @@ int main() {
     2.8 plus 4.3 would be 7 while the integer of 2.8 plus the integer of 4.3 would be 6.
     */
     
-    printf("%d\n", abc);
+    printf("%d\n", c);
     return 0; 
 }
