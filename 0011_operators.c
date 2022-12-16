854aa5db6aa15d6bcfc6176f6f58097e0907a43a
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-16 09:42:02 +0100

    Update 0011_operators.c

--- 0011_operators.c
+++ 0011_operators.c
@@ -1,3 +1,15 @@
 #include<stdio.h>
 
 int main() {
+    int a = 4;
+    int b = 3;
+
+    printf("a is %d.\n", a);
+    printf("b is %d.\n", b);
+  
+    /*The first kind of operator is the arithmetic operator
+    */
+    printf("a + b is %d.\n", a+b);
+  
+    
+}
