e9782dbc1d25694bc8d4e4291e7d7396b8f4ee81
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-27 15:54:57 +0100

    Create 0015_while_loop.c

---
+++ 0015_while_loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a = 4;
+    int b = -3;
+    
+    /*
+    The conditions and commands within the while loop can be arbitrarily complex.
+    But it is still important to make sure, that there is a valid abort condition if the loop should not 
+    run forever. E.g. the second loop in this script would do so as only one condition has to be True and
+    'b' does never change. To not make the script crash I commented this loop out. 
+    */
+  
+    while(a <= 20 && b == -3) {
+        printf("a is %d.\n", a);
+        a++;
+    }
+  
+    /*
+    while(a <= 20 || b == -3) {
+        printf("a is %d.\n", a);
+        a++;
+    }
+    */
+  
+    return 0; 
+}
