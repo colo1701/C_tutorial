16b02e9fe32a06880c6cc72323c4dac232d33ea2
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-27 16:02:22 +0100

    Create 0016_while_loop.c

---
+++ 0016_while_loop.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a = 4;
+    int b = -3;
+    
+    /*
+    It is also possible to perform other indented operations within the loop.
+    In this script the first loop stops after the first process of the outer loop as 'b' is not set back to -3.
+    The second loop shows the difference where the iteration generates an output for all upcoming values of 'a.
+    */
+  
+    while(a <= 20) {
+        while(b < 0) {
+            printf("a is %d and b is %d.\n", a, b);
+            b++;
+        }
+        a++;
+    }
+  
+    a = 4;
+    while(a <= 20) {
+        b = -3;
+        while(b < 0) {
+            printf("a is %d and b is %d.\n", a, b);
+            b++;
+        }
+        a++;
+    }
+  
+    return 0; 
+}
