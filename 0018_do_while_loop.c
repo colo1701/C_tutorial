1bbe2b7e2a37d4c1e427919112d51fee1efbae90
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-30 14:25:03 +0100

    Update 0018_do_while_loop.c

--- 0018_do_while_loop.c
+++ 0018_do_while_loop.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int a = 4;
-    int b = -3;
+    int a = 0;
   
+    do {
+        printf("a is %d.\n", a);
+        a++;
+    } while(a < 10);
   
     /*
-    comment section
+    The do while loop works at first sight like the while loop.
+    The difference is that if the variable defined in advance does not satisfy the condition 
+    of the do while loop, the commands are still executed once. The condition is therefore 
+    only checked after the first run.
     */
 
-
-
+    int b = 120;
+    
+    do {
+        printf("b is %d.\n", b);
+        b++;
+    } while(b < 10);
 
     return 0;
 }
