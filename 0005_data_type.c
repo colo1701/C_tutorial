b479e50a3487ff2cb4566e39ba9ea5b56cfe14c6
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-03 09:56:47 +0100

    Update 0005_data_type.c

--- 0005_data_type.c
+++ 0005_data_type.c
@@ -7,9 +7,10 @@ int main() {
     int c = a + b;
   
     /* 
-    Adding one (ore more) float values into an integer value will cause the integer variable to 
-    simply cutoff the decimal part of the float value AFTER the calculation is made. This means the integer of 
-    2.8 plus 4.3 would be 7 while the integer of 2.8 plus the integer of 4.3 would be 6.
+    Adding one (ore more) float values into an integer value will cause the 
+    integer variable to simply cutoff the decimal part of the float value AFTER 
+    the calculation is made. This means the integer of 2.8 plus 4.3 would be 7 
+    while the integer of 2.8 + the integer of 4.3 would be 6.
     */
     
     printf("%d\n", c);
