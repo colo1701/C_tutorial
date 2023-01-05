bd6328e2acc019b0818193c94de0d922394ba9d1
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-05 16:18:32 +0100

    Update 0010_if_query.c

--- 0010_if_query.c
+++ 0010_if_query.c
@@ -3,15 +3,17 @@
 int main() {
     int a = 0;
     int b = 2;
-    
+
     /*
-    The if query consists of 3 elements: the call, the condition and the command if the condition is true.
-    If the condition is the question whether 2 values are equal, it has to be asked using '==' because a single '='
-    is already used to define a value to a variable.
-    The simplest query asks for a single condition and will only return the command if the condition is true.
-    If the condition is false, the query will not cause any reply.
-    This query can be expanded by an 'else if' condition (if there's another condition to check) or an 'else'
-    argument (a reply that will show up, if none of the previous conditions are true).
+    The if query consists of 3 elements: the call, the condition and the command 
+    if the condition is true. If the condition is the question whether 2 values 
+    are equal, it has to be asked using '==' because a single '=' is already 
+    used to define a value to a variable. The simplest query asks for a single 
+    condition and will only return the command if the condition is true. If the 
+    condition is false, the query will not cause any reply. This query can be 
+    expanded by an 'else if' condition (if there's another condition to check) 
+    or an 'else' argument (a reply that will show up, if none of the previous 
+    conditions are true).
     */
     
     // This shows a simple if query:
