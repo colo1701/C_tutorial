f478c7b8f71a1ded001911b3ddfa7136029ce179
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-05 16:30:49 +0100

    Update 0019_loop_control.c

--- 0019_loop_control.c
+++ 0019_loop_control.c
@@ -6,13 +6,13 @@ int main() {
   
     /*
     Loop control describes two ways to control loops: break & continue.
-    The use takes place within an if branch in the loop. If the condition of the query is met, 
-    the 'break' command ends the loop completely and the code after the loop is executed.
-    The 'continue' command ensures that only the one pass of the loop is skipped. After that, 
-    the loop continues to be executed.
+    The use takes place within an if branch in the loop. If the condition of 
+    the query is met, the 'break' command ends the loop completely and the code 
+    after the loop is executed. The 'continue' command ensures that only the one 
+    pass of the loop is skipped. After that, the loop continues to be executed.
     In both cases, the order of the elements used in the loop must be observed.
-    In the following loop you can see that the output for the value 'a = 5' still occurs and 
-    then the loop is terminated.
+    In the following loop you can see that the output for the value 'a = 5' 
+    still occurs and then the loop is terminated.
     */
   
     for(a = 0; a < 10; a++) {
@@ -24,9 +24,9 @@ int main() {
     }
     
     /*
-    In this loop you can see the danger of the wrong sequence. If we are to skip the output for 
-    'b = 5', we must bind the output before it takes place. Therefore output and if query are 
-    swapped here compared to the upper loop.
+    In this loop you can see the danger of the wrong sequence. If we are to skip 
+    the output for 'b = 5', we must bind the output before it takes place. 
+    Therefore output and if query are swapped here compared to the upper loop.
     */
   
     for(b = 0; b < 10; b++) {
