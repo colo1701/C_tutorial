b4a8119389625191bcbe013321d45aa78f31fb0d
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-05 16:25:36 +0100

    Update 0014_while_loop.c

--- 0014_while_loop.c
+++ 0014_while_loop.c
@@ -3,10 +3,10 @@
 int main() {
     int a = 4;
     int b = -3;
-    
+
     /*
-    This while loop executes it's commands as long as the condition 'a > 0' is True. If we would not reduce
-    'a' within the loop, it would run forever.
+    This while loop executes it's commands as long as the condition 'a > 0' is 
+    True. If we would not reduce 'a' within the loop, it would run forever.
     */
   
     while(a > 0) {
