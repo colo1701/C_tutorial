5c64f86b54f9c12058cffbdd99aab8db5a56ade6
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-05 16:25:05 +0100

    Update 0013_switch_case.c

--- 0013_switch_case.c
+++ 0013_switch_case.c
@@ -2,14 +2,15 @@
 
 int main() {
     char grade = 'B';
-    
+
     /*
-    'switch case' is a very comfortable solution to list long branched if commands in a clearer way.
-    In the switch argument one needs to define the variable to be checked (in this case 'grade').
-    Afterwards on can easily list up all necessary cases and what to do if case is True.
-    If you want the switch case to stop after the first True case you have to add 'break;' to the case,
-    otherwise the sqitch will proceed running.
-    At the end of the case list you can define a default command that will be executed, if none of the 
+    'switch case' is a very comfortable solution to list long branched if 
+    commands in a clearer way. In the switch argument one needs to define the 
+    variable to be checked (in this case 'grade'). Afterwards on can easily list 
+    up all necessary cases and what to do if case is True. If you want the 
+    switch case to stop after the first True case you have to add 'break;' to 
+    the case, otherwise the sqitch will proceed running. At the end of the case 
+    list you can define a default command that will be executed, if none of the 
     cases outputs a True statement.
     */
     
@@ -35,8 +36,9 @@ int main() {
     }
     
     /*
-    If you want to run the same command on several cases you can list up all those commands right below each 
-    other and put the command only in the last case of this list. See the next switch case for an example.
+    If you want to run the same command on several cases you can list up all 
+    those commands right below each other and put the command only in the last 
+    case of this list. See the next switch case for an example.
     You can see that the string 'Still good' wil be put out in case B, C and D.
     */
     
