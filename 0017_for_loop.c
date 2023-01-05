cad4d623114723a9c1f9e9585958a77c1225a38f
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-05 16:29:05 +0100

    Update 0017_for_loop.c

--- 0017_for_loop.c
+++ 0017_for_loop.c
@@ -3,15 +3,15 @@
 int main() {
     /*
     while loops are good for counting but with for loops it is much easier.
-    At the beginning of the loop the conditions for the loop variable are defined. 
-    In this case, a should be 0 at the beginning, run the loop until a reaches the 
-    value 10 and increase by 1 with each pass.
-    The increase of the value of a always happens after the loop has run.
-    With the conditions 'start at 0', 'increment by 1' and 'stop at <n' you can cause 
-    exactly n runs of the loop, which is usually one of the two essential purposes 
-    (1. as a pure counting loop, 2. to link a certain content condition with a break).
-    Since the start value is defined in the loop, it is sufficient to determine only 
-    the data type of the variable in advance with 'int a'.
+    At the beginning of the loop the conditions for the loop variable are 
+    defined. In this case, a should be 0 at the beginning, run the loop until 
+    a reaches the value 10 and increase by 1 with each pass. The increase of the 
+    value of a always happens after the loop has run. With the conditions 
+    'start at 0', 'increment by 1' and 'stop at <n' you can cause exactly n runs 
+    of the loop, which is usually one of the two essential purposes (1. as a 
+    pure counting loop, 2. to link a certain content condition with a break).
+    Since the start value is defined in the loop, it is sufficient to determine 
+    only the data type of the variable in advance with 'int a'.
     */
   
     int a;
